d739cb1df3234cfc862e5d44c54be1cb8e87e7d7
Mario Espada <mario.espada77@gmail.com>
2019-10-24 05:13:36 +0200

    ADD auth system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PrivateView from './views/PrivateView';
+import Login from './views/auth/Login';
+import Signup from './views/auth/Signup';
+import { withAuth } from './Context/AuthContext';
+
+import PrivateRoute from './components/PrivateRoute';
+import AnonRoute from './components/AnonRoute';
+
+class App extends Component {
+  render() {
+    const { handleLogout } = this.props;
+    return (
+      <>
+          <button onClick={handleLogout}>logout</button>
+          <Router>
+            <AnonRoute exact path="/login" component={Login} />
+            <AnonRoute exact path="/signup" component={Signup} />
+            <PrivateRoute exact path="/private" component={PrivateView} />
+          </Router>
+      </>
+    );
+  }
 }
 
-export default App;
+export default withAuth(App);
