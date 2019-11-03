98f35693ad4cd17690305a710c028ef033a641c9
Mario Espada <mario.espada77@gmail.com>
2019-11-03 23:02:23 +0100

    ADD comprobaciones follow/unfollow listFollowers

--- frontend/src/components/ListFollowers.js
+++ frontend/src/components/ListFollowers.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { withAuth } from "../Context/AuthContext";
+import followServices from "../services/followService";
 import Follow from "./Follow";
 
 class ListFollowers extends Component {
     state = {
         isFollowing: null,
         isOtherUser: null,
-        following: {}
+        ifFollwing: {}
       };
      async componentDidMount(){
         const { username, user } = this.props;
-        const isFollowing = username.follower === user._id && this.setState({ isFollowing: true });
-      }
+        const isFollowing = username.followed === user._id && this.setState({ isFollowing: true });
+        const following = await followServices.getFollowing(user.username);
+        console.log("following" , following)
+        const ifFollwing = await following.find(element => {
+            console.log("element", element.followed._id)
+            console.log("USERNAME", username.follower._id)
+            if (element.followed._id === username.follower._id) {
+              return true;
+            }
+          });
+        if (ifFollwing) {
+            this.setState({
+              isFollowing: true,
+              isOtherUser: true,
+              following: ifFollwing
+            });
+        }
+     }
+      
     render() {
         const { isFollowing } = this.state;
         const { username } = this.props;
