c852b91acd17eda0346b64d1b4540d4c22145e8f
Mario Espada <mario.espada77@gmail.com>
2019-11-05 21:28:06 +0100

    ADD university to writePost component

--- frontend/src/components/WritePost.js
+++ frontend/src/components/WritePost.js
@@ -1,41 +1,48 @@
-import React, { Component } from 'react';
-import postServices from '../services/postService';
+import React, { Component } from "react";
+import postServices from "../services/postService";
 
 class WritePost extends Component {
-    state = {
-        text: '',
-    }
-     handleInput = (e) =>{
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
-    addPost = (e) =>{
-        e.preventDefault();
-        const { user, updatePost } = this.props;
-        const { text } = this.state;
-        const username = user.username;
-        postServices.createPost(username, text)
-        .then((post) =>{
-            updatePost(post.post)
-        })
-        .catch((error) =>{
-            console.log(error);
-        })
-    }
+  state = {
+    text: ""
+  };
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  addPost = e => {
+    e.preventDefault();
+    const { user, updatePost, university } = this.props;
+    const { text } = this.state;
+    const username = user.username;
+    postServices
+      .createPost(username, text, university)
+      .then(post => {
+        updatePost(post.post);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-    render() {
-        const { addPost } = this.props;
-        const { text } = this.state;
-        return (
-            <div>
-                <form onSubmit={this.addPost}>
-                    <input type="text" name="text" placeholder="En que estás pensando?" value={text} onChange={this.handleInput}></input>
-                    <button>Publicar</button>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    const { addPost } = this.props;
+    const { text } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.addPost}>
+          <input
+            type="text"
+            name="text"
+            placeholder="En que estás pensando?"
+            value={text}
+            onChange={this.handleInput}
+          ></input>
+          <button>Publicar</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default WritePost;
