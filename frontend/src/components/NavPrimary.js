25b063a17eaa96b8c4cc18238f65406b612db9b2
Mario Espada <mario.espada77@gmail.com>
2019-11-08 03:52:58 +0100

    ADD see user profile in nav component

--- frontend/src/components/NavPrimary.js
+++ frontend/src/components/NavPrimary.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { withAuth } from "../Context/AuthContext";
+import { Link } from "react-router-dom";
 import "../css/nav.css";
 
 class nav extends Component {
     render() {
+        const { user } = this.props;
         return (
             <div>
-                <div className="sticky-top primary-color padding-nav">Sticky top</div>
+                <div className="sticky-top primary-color padding-nav">
+                    <div className="username-profile">
+                    <Link to={`/profile/${user.username}`}>{user.username}</Link>
+                    </div>
+                </div>
             </div>
         );
     }
