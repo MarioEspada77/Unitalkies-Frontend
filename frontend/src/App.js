c0943700d1466b718a3a34dd9266e08cbdfbdf49
Mario Espada <mario.espada77@gmail.com>
2019-11-09 15:00:11 +0100

    Change button logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import PrivateView from './views/PrivateView';
-import Login from './views/auth/Login';
-import Signup from './views/auth/Signup';
-import { withAuth } from './Context/AuthContext';
+import PrivateView from "./views/PrivateView";
+import Login from "./views/auth/Login";
+import Signup from "./views/auth/Signup";
+import { withAuth } from "./Context/AuthContext";
 
-import PrivateRoute from './components/PrivateRoute';
-import AnonRoute from './components/AnonRoute';
-import Home from './views/Home';
-import UserProfileView from './views/UserProfileView';
-import UserFollowers from './components/UserFollowers';
-import UserFollowing from './components/UserFollowing';
+import PrivateRoute from "./components/PrivateRoute";
+import AnonRoute from "./components/AnonRoute";
+import Home from "./views/Home";
+import UserProfileView from "./views/UserProfileView";
+import UserFollowers from "./components/UserFollowers";
+import UserFollowing from "./components/UserFollowing";
 
 class App extends Component {
   render() {
     const { handleLogout } = this.props;
     return (
       <>
-          <button onClick={handleLogout}>logout</button>
-          <Router>
-            <AnonRoute exact path="/login" component={Login} />
-            <AnonRoute exact path="/signup" component={Signup} />
-            <PrivateRoute exact path="/home" component={Home} />
-            <PrivateRoute exact path="/profile/:username" component={UserProfileView} />
-            <PrivateRoute exact path="/followers/:username" component={UserFollowers} />
-            <PrivateRoute exact path="/following/:username" component={UserFollowing} />
-          </Router>
+        <Router>
+          <AnonRoute exact path="/login" component={Login} />
+          <AnonRoute exact path="/signup" component={Signup} />
+          <PrivateRoute exact path="/home" component={Home} />
+          <PrivateRoute
+            exact
+            path="/profile/:username"
+            component={UserProfileView}
+          />
+          <PrivateRoute
+            exact
+            path="/followers/:username"
+            component={UserFollowers}
+          />
+          <PrivateRoute
+            exact
+            path="/following/:username"
+            component={UserFollowing}
+          />
+        </Router>
       </>
     );
   }
--- frontend/src/components/NavPrimary.js
+++ frontend/src/components/NavPrimary.js
@@ -5,15 +5,17 @@ import "../css/nav.css";
 
 class nav extends Component {
   render() {
-    const { user } = this.props;
+    const { user, handleLogout } = this.props;
     return (
       <>
         <div className="fixed-top primary-color padding-nav d-flex">
-          <div class="p-2 mr-auto ">Unitalkies</div>
-          <div class="p-2 ">Notificaciones</div>
-          <div class="p-2 ">
-            {" "}
+          <div className="p-2 mr-auto ">Unitalkies</div>
+          <div className="p-2 ">Notificaciones</div>
+          <div className="p-2 ">
             <Link to={`/profile/${user.username}`}>{user.username}</Link>
+          </div>
+          <div className="p-2">
+            <button onClick={handleLogout}>logout</button>
           </div>
         </div>
       </>
