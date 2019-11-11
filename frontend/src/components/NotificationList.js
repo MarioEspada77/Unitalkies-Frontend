2a8714533250e81c8147c09783542d368c20b292
Mario Espada <mario.espada77@gmail.com>
2019-11-11 17:27:35 +0100

    ADD list notification

--- frontend/src/components/NotificationList.js
+++ frontend/src/components/NotificationList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class NotificationList extends Component {
+
+   async componentDidMount(){
+        const { notification } = this.props;
+        
+    }
     render() {
+        const { notification } = this.props;
         return (
             <div>
-                
+            <p>{notification.notificationFrom.username} {notification.text}</p>
             </div>
         );
     }
