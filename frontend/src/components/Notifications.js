9cebb89fb14d78344b080681a530a3fd4e1d42ee
Mario Espada <mario.espada77@gmail.com>
2019-11-11 17:11:48 +0100

    ADD notifications map in notification component

--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import notifificationServices from "../services/notificationService";
+import NotificationList from './NotificationList';
 
 class Notifications extends Component {
+    state = {
+        notifications: [],
+      }
+      async componentDidMount(){
+        const notifications = await notifificationServices.getAllNotifications();
+       this.setState({
+         notifications,
+       })
+      }
     render() {
+        const { notifications } = this.state;
         return (
             <div>
-                
+                {notifications.map((notification) =>{
+                    return <NotificationList notification={notification}/>
+                })}
             </div>
         );
     }
