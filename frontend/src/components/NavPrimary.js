5d4734502b21d4059edcb39d2732ef7ccd3d566c
Mario Espada <mario.espada77@gmail.com>
2019-11-12 17:35:17 +0100

    ADD dropdown to menu

--- frontend/src/components/NavPrimary.js
+++ frontend/src/components/NavPrimary.js
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 import "../css/nav.css";
 import "../css/notification.scss";
 import notifificationServices from "../services/notificationService";
+import "../dropdown.js";
 
 class nav extends Component {
   state = {
-    notifications: [],
-  }
-  async componentDidMount(){
+    notifications: []
+  };
+  async componentDidMount() {
     const notifications = await notifificationServices.getAllNotifications();
-   this.setState({
-     notifications,
-   })
+    this.setState({
+      notifications
+    });
   }
   render() {
     const { user, handleLogout } = this.props;
@@ -24,16 +25,34 @@ class nav extends Component {
           <div className="p-2 mr-auto ">Unitalkies</div>
           <div className="p-2 ">
             <Link to={`/notifications/all`}>
-                <i className="fa fa-bell notification">
-                  {notifications.length > 0 && <span className="badge">{notifications.length}</span> }     
-                 </i>
+              <i className="fa fa-bell notification">
+                {notifications.length > 0 && (
+                  <span className="badge">{notifications.length}</span>
+                )}
+              </i>
             </Link>
           </div>
           <div className="p-2 ">
-            <Link to={`/profile/${user.username}`}>{user.username}</Link>
-          </div>
-          <div className="p-2">
-            <button onClick={handleLogout}>logout</button>
+            <div class="btn-group">
+              <button
+                type="button"
+                class=" dropdown-toggle"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                {user.username}
+              </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item">
+                  <Link to={`/profile/${user.username}`}>Ver perfil</Link>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" onClick={handleLogout}>
+                  Cerrar sesión
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </>
--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -48,15 +48,21 @@ class Home extends Component {
     if (posts.length === 0) {
       return (
         <>
-           <div className="shadow-sm p-3 mb-5 rounded card-color"><WritePost user={user} updatePost={this.updatePost} /></div>
-           <div className="shadow-sm p-3 mb-5 rounded card-color">No hay ninguna publicación que mostrar</div>
+          <div className="shadow-sm p-3 mb-5 rounded card-color">
+            <WritePost user={user} updatePost={this.updatePost} />
+          </div>
+          <div className="shadow-sm p-3 mb-5 rounded card-color">
+            No hay ninguna publicación que mostrar
+          </div>
         </>
       );
     } else {
       console.log(posts);
       return (
         <>
-          <div className="shadow-sm p-3 mb-5 rounded card-color"><WritePost user={user} updatePost={this.updatePost} /></div>
+          <div className="shadow-sm p-3 mb-5 rounded card-color">
+            <WritePost user={user} updatePost={this.updatePost} />
+          </div>
           <Post posts={posts}></Post>
         </>
       );
@@ -69,46 +75,55 @@ class Home extends Component {
     return (
       <div className="container-fluid">
         <NavPrimary />
-          <div className="row">
-            <div className="col-md-4 ">
+        <div className="row">
+          <div className="col-md-4 ">
             <div className="user-card margin-home">
-                 <div className="container">
-                    <div className="information">
-                        <img src={image} alt="avatar" className="avatar rounded-circle" width="100" />
-                        <div className="name">@{user.username}</div>
-                        <div className="position">Universidad</div>
-      
-                      <div className="stats">
-                          <span className="followers">
-                              <span className="value">0</span>
-                              <span className="label">Seguidores</span>
-                         </span> 
-        
-                          <span className="following">
-                              <span className="value">0</span>
-                              <span className="label">Siguiendo</span>
-                          </span>
-        
-                          <span className="stories">
-                              <span className="value">{userPosts.length}</span>
-                              <span className="label">Post</span>
-                         </span>
-                      </div> 
-                     </div> 
-                   </div> 
-            </div>
-          </div>
-            <div className="col-md-6 margin-home">
-              {loading && <div className="loading"><div className="spinner-border loading"></div></div>}
-              {!error ? (
-                !loading && this.statusPosts()
-              ) : (
-                <div className ="shadow-sm p-3 mb-5 rounded card-color">
-                  <p>{error}</p>
+              <div className="container">
+                <div className="information">
+                  <img
+                    src={image}
+                    alt="avatar"
+                    className="avatar rounded-circle"
+                    width="100"
+                  />
+                  <div className="name">@{user.username}</div>
+                  <div className="position">Universidad</div>
+
+                  <div className="stats">
+                    <span className="followers">
+                      <span className="value">0</span>
+                      <span className="label">Seguidores</span>
+                    </span>
+
+                    <span className="following">
+                      <span className="value">0</span>
+                      <span className="label">Siguiendo</span>
+                    </span>
+
+                    <span className="stories">
+                      <span className="value">{userPosts.length}</span>
+                      <span className="label">Post</span>
+                    </span>
+                  </div>
                 </div>
-              )} 
+              </div>
             </div>
           </div>
+          <div className="col-md-6 margin-home">
+            {loading && (
+              <div className="loading">
+                <div className="spinner-border loading"></div>
+              </div>
+            )}
+            {!error ? (
+              !loading && this.statusPosts()
+            ) : (
+              <div className="shadow-sm p-3 mb-5 rounded card-color">
+                <p>{error}</p>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
