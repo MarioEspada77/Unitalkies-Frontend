fff9bed4dfa29e3b2cb545ca11ca9e42666378eb
Mario Espada <mario.espada77@gmail.com>
2019-11-15 18:38:33 +0100

    ADD navbar to login and signup

--- src/components/NavPrimary.js
+++ src/components/NavPrimary.js
@@ -39,32 +39,47 @@ class nav extends Component {
       <>
       <NavColor className="fixed-top padding-nav d-flex">
           <div className="p-2 mr-auto ">Unitalkies</div>
-          <div className="p-2 ">
-            <Link to={`/notifications/all`}>
-              <i className="fa fa-bell notification">
-                {notifications.length > 0 && (
-                  <span className="badge">{notifications.length}</span>
-                )}
-              </i>
-            </Link>
-          </div>
-          <div className="p-2 ">
-            <div class="btn-group">
-            <img src={image} alt="dropdown-toggle" className="rounded-circle dropdown-toggle" width="40" data-toggle="dropdown" aria-haspopup="true" ria-expanded="false"/>
-              <DropDown className="dropdown-menu">
-                <a class="dropdown-item">
-                  <Link to={`/profile/${user.username}`}>Ver perfil</Link>
-                </a>
-                <a className="dropdown-item">
-                <a onClick={this.handleChangeTheme}>{actualTheme.theme === "light" ? "Cambiar a modo oscuro" : "Cambiar a modo claro"}</a>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" onClick={handleLogout}>
-                  Cerrar sesión
-                </a>
-            </DropDown>
-            </div>
-          </div>
+            <>
+            {user ?
+            <>
+              <div className="p-2 ">
+                  <Link to={`/notifications/all`}>
+                      <i className="fa fa-bell notification">
+                          {notifications.length > 0 && (
+                            <span className="badge">{notifications.length}</span>
+                          )}
+                      </i>
+                  </Link>
+                </div>
+                <div className="p-2 ">
+                    <div class="btn-group">
+                        <img src={image} alt="dropdown-toggle" className="rounded-circle dropdown-toggle" width="40" data-toggle="dropdown" aria-haspopup="true" ria-expanded="false"/>
+                            <DropDown className="dropdown-menu">
+                                  <a class="dropdown-item">
+                                    <Link to={`/profile/${user.username}`}>Ver perfil</Link>
+                                  </a>
+                                  <a className="dropdown-item">
+                                    <a onClick={this.handleChangeTheme}>{actualTheme.theme === "light" ? "Cambiar a modo oscuro" : "Cambiar a modo claro"}</a>
+                                  </a>
+                                 <div class="dropdown-divider"></div>
+                                 <a class="dropdown-item" onClick={handleLogout}>
+                                    Cerrar sesión
+                                  </a>
+                            </DropDown>
+                     </div>
+                 </div> 
+                </>
+              : 
+              <>
+              <div className="p-2">
+                <Link to={`/login`}>Login</Link>
+              </div>
+              <div className="p-2">
+                <Link to={`/login`}>Signup</Link>
+              </div>
+              </>
+                }
+        </>
         </NavColor>
       </>
     );
