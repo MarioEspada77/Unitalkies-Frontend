26138f780ea6fc9554acf6648ae45d494309b09f
Mario Espada <mario.espada77@gmail.com>
2019-11-16 12:05:16 +0100

    ADD 404 component

--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const NotFound = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  margin-top: 100px;
+  text-align: center;
+  padding: 10px;
+  background-color: ${({ theme }) => theme.boxColor};
+  width: 40%;
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  color: ${({ theme }) => theme.color};
+`;
 
 class NoPage extends Component {
   render() {
     return (
       <div>
-        <p>Página no encontarda.</p>
+        <NotFound>
+          <h1>UPS!</h1>
+          <p>Página no encontarda.</p>
+        </NotFound>
       </div>
     );
   }
